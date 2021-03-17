8f2a25feb3305a0da0bd9c6f6300ca2116935873
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-03-17 09:00:38 +0530

    Updated the controller function call
    Updated the controller function call

--- checker.c
+++ checker.c
@@ -3,55 +3,61 @@
 #include <assert.h>
 #include "BatteryManagementConfig.h"
 
+int MsgBufferLength = 200;
 
-int alertIfBatteryInCriticalRange(float,BatteryManagementConfig);
-int warnIfBatteryInWarningRange(float,BatteryManagementConfig);
+extern int mainController(int BatteryHealthStatus,char AlertMessage[]);
+
+int alertIfBatteryInCriticalRange(float, BatteryManagementConfig, char*);
+int warnIfBatteryInWarningRange(float, BatteryManagementConfig, char*);
 
 
-int alertIfBatteryIsUnHealthy(float MeasuredValue, BatteryManagementConfig MinMaxMessage)
+void alertIfBatteryIsUnHealthy(float MeasuredValue, BatteryManagementConfig MinMaxMessage,int * Health_State, char* Message_Buffer)
 {
-	if(alertIfBatteryInCriticalRange(MeasuredValue,MinMaxMessage)) 
+
+	if(alertIfBatteryInCriticalRange(MeasuredValue,MinMaxMessage, Message_Buffer)) 
 	{
-	return HEALTH_NOT_OK;
+	*Health_State = HEALTH_NOT_OK;
 	}
-	else if(warnIfBatteryInWarningRange(MeasuredValue,MinMaxMessage))
+	else if(warnIfBatteryInWarningRange(MeasuredValue,MinMaxMessage, Message_Buffer))
 	{
-	return HEALTH_NOT_OK;
+	*Health_State =  HEALTH_NOT_OK;
 	}
-	printf("\n\t%s", MinMaxMessage.Alertmessage.Normal_Alertmessage[Alert_Language]);
-	return HEALTH_OK;
+	else
+	{
+	sprintf(Message_Buffer, "%s\n\t%s",Message_Buffer, MinMaxMessage.Alertmessage.Normal_Alertmessage[Alert_Language]);
+	}
 
 }
 
-int alertIfBatteryInCriticalRange(float MeasuredValue, BatteryManagementConfig CriticalRange)
+int alertIfBatteryInCriticalRange(float MeasuredValue, BatteryManagementConfig CriticalRange, char* Message_Buffer)
 {
 	if(MeasuredValue < CriticalRange.Limit.Low_Critical_range) 
 	{
-    printf("\n\t%s", CriticalRange.Alertmessage.Low_Critical_Alertmessage[Alert_Language]);
+	sprintf(Message_Buffer, "%s\n\t%s",Message_Buffer, CriticalRange.Alertmessage.Low_Critical_Alertmessage[Alert_Language]);
 	return 1;
 	}
 	else if(MeasuredValue > CriticalRange.Limit.High_Critical_range) 
 	{
-    printf("\n\t%s", CriticalRange.Alertmessage.High_Critical_Alertmessage[Alert_Language]);
+	sprintf(Message_Buffer, "%s\n\t%s",Message_Buffer,CriticalRange.Alertmessage.High_Critical_Alertmessage[Alert_Language]);
 	return 1;
 	}
     return 0;
 
 }
 
-int warnIfBatteryInWarningRange(float MeasuredValue, BatteryManagementConfig WarningRange)
+int warnIfBatteryInWarningRange(float MeasuredValue, BatteryManagementConfig WarningRange , char* Message_Buffer)
 {
 	if(MeasuredValue < WarningRange.Limit.Low_Warning_range) 
 	{
-    printf("\n\t%s", WarningRange.Alertmessage.Low_Warning_Alertmessage[Alert_Language]);
+	sprintf(Message_Buffer, "%s\n\t%s",Message_Buffer,WarningRange.Alertmessage.Low_Warning_Alertmessage[Alert_Language]);
 	return 1;
 	}
 	else if(MeasuredValue > WarningRange.Limit.High_Warning_range) 
 	{
-    printf("\n\t%s", WarningRange.Alertmessage.High_Warning_Alertmessage[Alert_Language]);
+	sprintf(Message_Buffer, "%s\n\t%s",Message_Buffer,WarningRange.Alertmessage.High_Warning_Alertmessage[Alert_Language]);
 	return 1;
 	}
-    return 0;
+    	return 0;
 
 }
 
@@ -61,22 +67,25 @@ int warnIfBatteryInWarningRange(float MeasuredValue, BatteryManagementConfig War
 
 int batteryIsOk(float temperature, char temperature_unit, float soc, float chargeRate) 
 {
+	
+	int Health_Status = HEALTH_OK;
+	char Message_Buffer[MsgBufferLength];
+	Message_Buffer[0]='\0';       // Clear the Buffer
+	
 	if(temperature_unit=='F')
-	{
+		{
 		temperature=((temperature-32) * ((float)5/9));   // Converting Fahrenheit to Celsius
-	}
+		}
 	else
-	{
+		{
 		temperature=temperature;
-	}
-	
-	
-	return 	(
-	
-	alertIfBatteryIsUnHealthy(temperature, temperature_config) & \
-	alertIfBatteryIsUnHealthy(soc, soc_config) & \
-	alertIfBatteryIsUnHealthy(chargeRate, chargeRate_config));
-			
+		}
+
+	alertIfBatteryIsUnHealthy(temperature, temperature_config, &Health_Status, Message_Buffer );
+	alertIfBatteryIsUnHealthy(soc, soc_config, &Health_Status, Message_Buffer);
+	alertIfBatteryIsUnHealthy(chargeRate, chargeRate_config, &Health_Status, Message_Buffer);
+
+	return mainController(Health_Status,Message_Buffer);
 }
 
 int main() {
@@ -88,7 +97,7 @@ int main() {
 #define TEST_CASE(a) printf("\nTest_Case_%d",a);
 
 
-TEST_CASE(1);  	assert( batteryIsOk( 68, 'F', 50,  0.4));	// Test Battery is Healthy
+TEST_CASE(1);  	assert( batteryIsOk(  68, 'F', 50,  0.4));	// Test Battery is Healthy
   
 TEST_CASE(2);  	assert(!batteryIsOk( 50, 'C', 50,  0.4)); 	//Test Battery Temperature is higher than critical error limit
 TEST_CASE(3);  	assert(!batteryIsOk( 20, 'C',  90,  0.4)); 	//Test Battery State-of-Charge is higher than critical error limit
@@ -105,3 +114,4 @@ TEST_CASE(12);  assert(!batteryIsOk( 20, 'C',  10,  0.4)); 	//Test Battery State
 TEST_CASE(13);  assert(!batteryIsOk( 20, 'C',  50,  0.1));	//Test Battery Charge-rate is lower than warning limit and higher than critical error limit
   
 }
+
